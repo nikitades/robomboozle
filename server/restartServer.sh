93527fe39912493ee3751c1e6dc8d2927913eccc
nikitades <nikitades@pm.me>
2020-02-06 17:39:42 +0300

    WIP: It starts to work out

--- server/restartServer.sh
+++ server/restartServer.sh
@@ -4,5 +4,6 @@
 [ -z "$CIRCLE_BRANCH" ] || (echo "Define the branch name!" && exit)
 [ -z "$CIRCLE_SHA1" ] || (echo "Define the commit hash!" && exit)
 [ -z "$SERVICE_NAME" ] || (echo "Define the service name!" && exit)
-docker rmi $(docker image ls | grep nikitades/robomboozle-server:*)
+docker image ls | grep "$DOCKERHUB_USERNAME"/"$DOCKER_IMAGE_NAME":*
+docker rmi $(docker image ls | "$DOCKERHUB_USERNAME"/"$DOCKER_IMAGE_NAME")
 docker service update --image "$DOCKERHUB_USERNAME"/"$DOCKER_IMAGE_NAME":"$CIRCLE_BRANCH"-"$CIRCLE_SHA1" "$SERVICE_NAME"
