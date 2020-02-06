12529be4ca749c017d0881ade9c453ef3453b16f
nikitades <nikitades@pm.me>
2020-02-06 18:03:28 +0300

    WIP: Testing

--- server/restartServer.sh
+++ server/restartServer.sh
@@ -4,6 +4,9 @@
 [ -z "$CIRCLE_BRANCH" ] || (echo "Define the branch name!" && exit)
 [ -z "$CIRCLE_SHA1" ] || (echo "Define the commit hash!" && exit)
 [ -z "$SERVICE_NAME" ] || (echo "Define the service name!" && exit)
+echo $DOCKERHUB_USERNAME
+echo $DOCKER_IMAGE_NAME
 docker image ls | grep "$DOCKERHUB_USERNAME"/"$DOCKER_IMAGE_NAME":*
+docker rmi $(docker image ls | grep f)
 docker rmi $(docker image ls | "$DOCKERHUB_USERNAME"/"$DOCKER_IMAGE_NAME")
 docker service update --image "$DOCKERHUB_USERNAME"/"$DOCKER_IMAGE_NAME":"$CIRCLE_BRANCH"-"$CIRCLE_SHA1" "$SERVICE_NAME"
